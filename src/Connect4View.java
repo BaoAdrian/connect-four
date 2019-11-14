551593fbd25fa66ce319f9f376e078e3cf57449d
Mauricio Herrera <mauherrerag@email.arizona.edu>
2019-11-14 13:17:08 -0700

    impemented inheritance

--- src/Connect4View.java
+++ src/Connect4View.java
@@ -1,4 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Connect4View {
+public class Connect4View extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
