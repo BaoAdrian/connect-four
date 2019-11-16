968b69f8953c6a5481939db0f3260d42424132fc
Adrian Bao <adrianbao14@gmail.com>
2019-11-16 14:17:28 -0700

    Add JUnit testing suite

---
+++ src/Connect4Test.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+
+public class Connect4Test {
+	
+	@Test
+	public void testCheckRows() {
+		Connect4Controller controller = new Connect4Controller();
+		
+	}
+	
+	@Test
+	public void testCheckCols() {
+		
+	}
+	
+	@Test
+	public void testCheckDiagonals() {
+		
+	}
+	
+	@Test
+	public void testCheckIfGameOver() {
+		
+	}
+	
+	@Test
+	public void testCheckIfValidMove() {
+		
+	}
+
+	@Test
+	public void testComputerTurn() {
+		
+	}
+	
+	@Test
+	public void testFullBoard() {
+		
+	}
+}
