2d16051548d012443b712b01bd7b9eeb3bfb9ddd
Mauricio Herrera <mauherrerag@email.arizona.edu>
2019-11-16 16:59:03 -0700

    Added connect method

--- src/Connect4Client.java
+++ src/Connect4Client.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.ConnectException;
 import java.net.Socket;
 
 public class Connect4Client {
@@ -9,16 +10,25 @@ public class Connect4Client {
 	private ObjectOutputStream output;
 	private ObjectInputStream input;
 	
-	// Constructor
-	public Connect4Client() {
+	
+	// Returns true if a successful connection was established.
+	public boolean connect(){
 		try {
 			server = new Socket(Connect4Server.HOST, Connect4Server.PORT);
 			output = new ObjectOutputStream(server.getOutputStream());
 			input = new ObjectInputStream(server.getInputStream());
+			return true;
+		} catch (ConnectException e) {
+			System.out.println("Error, server not initialized yet.");
+			return false;
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return false;
 		}
+	}
+	
+	public boolean isConnected() {
+		return server != null;
 	}
 	
 	public void sendMessage(Connect4MoveMessage message) {
