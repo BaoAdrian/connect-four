c7f267b82ce711cb6b3f28660cd100608d21d457
Mauricio Herrera <mauherrerag@email.arizona.edu>
2019-11-16 16:26:17 -0700

    Added initial fields and constructor to server and client

--- src/Connect4Client.java
+++ src/Connect4Client.java
@@ -1,4 +1,41 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
 public class Connect4Client {
-
+	// Fields
+	private Socket server;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	
+	// Constructor
+	public Connect4Client() {
+		try {
+			server = new Socket(Connect4Server.HOST, Connect4Server.PORT);
+			output = new ObjectOutputStream(server.getOutputStream());
+			input = new ObjectInputStream(server.getInputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendMessage(Connect4MoveMessage message) {
+		try {
+			output.writeObject(message);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void closeServer() {
+		try {
+			server.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
