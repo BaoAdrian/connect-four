61eda2791640f2b9066338acfa56e00bf5c44b1d
Adrian Bao <adrianbao14@gmail.com>
2019-11-17 10:36:32 -0700

    Add documentation

--- src/Connect4Model.java
+++ src/Connect4Model.java
@@ -58,10 +58,21 @@ public class Connect4Model extends java.util.Observable {
 		return board;
 	}
 	
+	/**
+	 * Public Accessor method for isOver
+	 * @return Boolean isOver attribute
+	 */
 	public boolean isOver() {
 		return isOver;
 	}
 	
+	/**
+	 * Performs an update to the isOver flag and notifies
+	 * all observers of the change in game state so that 
+	 * the Observers can update accordingly.
+	 * 
+	 * @param winningId Integer id of the winner
+	 */
 	public void updateToGameOver(Integer winningId) {
 		isOver = true;
 		setChanged();
