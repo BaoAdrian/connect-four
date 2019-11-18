21b8e9a67b31000943880069dc3e4110be702a79
Mauricio Herrera <mauherrerag@email.arizona.edu>
2019-11-18 01:19:19 -0700

    Added class header

--- src/Connect4.java
+++ src/Connect4.java
@@ -1,9 +1,21 @@
+/**
+ * @author Mauricio Herrera, Adrian Bao
+ * 
+ * CSC 335 - Object Oriented Programming and Design
+ * 
+ * Title: Networked Connect 4
+ * 
+ * Description: This class launches the GUI for the Connect 4 game.
+ */
 import javafx.application.Application;
 
 public class Connect4 {
 
+	/**
+	 * This method initializes view class.
+	 * @param args command line arguments
+	 */
 	public static void main(String[] args) {
-		// Creates controller
 		Application.launch(Connect4View.class, args);
 	}
 
