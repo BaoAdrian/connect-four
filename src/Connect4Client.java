f3d7a2fa4eaff0f18aa0a3715c2cfabba8b388be
Mauricio Herrera <mauherrerag@email.arizona.edu>
2019-11-18 01:30:23 -0700

    added comments

--- src/Connect4Client.java
+++ src/Connect4Client.java
@@ -1,8 +1,20 @@
+/**
+ * @author Mauricio Herrera, Adrian Bao
+ * 
+ * CSC 335 - Object Oriented Programming and Design
+ * 
+ * Title: Networked Connect 4
+ * 
+ * File: Connect4Client.java
+ * 
+ * Description: This class creates a client connection and handles
+ * communications between server and client.
+ */
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -26,28 +38,28 @@ public class Connect4Client {
 		message = null;
 	}
 	
-	
-	// Returns true if a successful connection was established.
-	public boolean connect(){
+	/**
+	 * This method connects to the server indicated by the host and port
+	 * indicated in the constructor. If a connection cannot be established,
+	 * an alert is displayed to the user.
+	 */
+	public void connect(){
 		try {
 			server = new Socket(host, port);
-//			output = new ObjectOutputStream(server.getOutputStream());
-//			input = new ObjectInputStream(server.getInputStream());
-			return true;
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("Error, server not initialized yet.");
 			Alert noServerFoundAlert = new Alert(AlertType.WARNING);
 			noServerFoundAlert.setContentText("Server not found! Please wait for server to start, and try again.");
 			noServerFoundAlert.showAndWait();
-			return false;
 		}
 	}
 	
-	public boolean isConnected() {
-		return server != null;
-	}
-	
+	/**
+	 * This method transmits a Connect4MoveMessage object to the
+	 * server in a new thread.
+	 * @param message move message to be transmitted
+	 */
 	public void sendMessage(Connect4MoveMessage message) {
 		RunnableSendMessage runnableMsg = new RunnableSendMessage(message);
 		Thread messageThread = new Thread(runnableMsg);
@@ -55,11 +67,18 @@ public class Connect4Client {
 		messageThread.start();
 	}
 	
+	/**
+	 * This method creates a new thread to wait for a Connect4MoveMessage to 
+	 * be transmitted from the server.
+	 */
 	public void waitForMessage() {
 		Thread incomingMessage = new Thread(new RunnableGetMessage());
 		incomingMessage.start();
 	}
 	
+	/**
+	 * This method closes the server socket.
+	 */
 	public void closeServer() {
 		try {
 			server.close();
@@ -69,7 +88,19 @@ public class Connect4Client {
 		}
 	}
 	
+	/**
+	 * 
+	 * @author Mauricio Herrera, Adrian Bao
+	 * 
+	 * Description: This class adds an implementation to the run
+	 * method in the Runnable class to be executed in a separate thread.
+	 *
+	 */
 	private class RunnableGetMessage implements Runnable {
+		/**
+		 * This method gets a Move message transmitted by the server, 
+		 * then it calls for the controller to handle the message.
+		 */
 		@Override
 		public void run() {
 			try {
@@ -94,12 +125,26 @@ public class Connect4Client {
 		}
 	}
 	
+	/**
+	 * 
+	 * @author Mauricio Herrera, Adrian Bao
+	 * 
+	 * Description: This class adds an implementation to the run
+	 * method in the Runnable class to be executed in a separate thread.
+	 * 
+	 */
 	private class RunnableSendMessage implements Runnable {
+		// Class fields
 		private Connect4MoveMessage message;
 		
+		// Constructor
 		public RunnableSendMessage(Connect4MoveMessage message) {
 			this.message = message;
 		}
+		
+		/**
+		 * This method sends a move message to the server.
+		 */
 		@Override
 		public void run() {
 			try {
